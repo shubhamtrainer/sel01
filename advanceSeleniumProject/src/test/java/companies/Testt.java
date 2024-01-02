233554a6ddf1e96510975ec67c2fe3146bb05c74
kumar <kumar@LavanyaKumar>
2024-01-02 16:48:25 +0530

    4:45

---
+++ advanceSeleniumProject/src/test/java/companies/Testt.java
@@ -0,0 +1,11 @@
+package companies;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testt {
+	@Test
+	public void DTest() {
+		Reporter.log("4:45 edited",true);
+	}
+}
